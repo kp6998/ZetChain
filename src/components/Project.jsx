e261b780be8ec0698071659cc45a4ed173853946
prakash-esf <Prakash.Kathiresan@esolutionsfirst.com>
2024-09-18 16:29:24 +0530

    update

---
+++ src/components/Project.jsx
@@ -0,0 +1,9 @@
+const Project = () => {
+    return(
+        <div className="not-found-container">
+            <h1>Soon!!!</h1>
+        </div>
+    )
+}
+
+export default Project;
---
+++ src/components/Team.jsx
@@ -0,0 +1,85 @@
+const Team = () => {
+    return (
+        <div className="team">
+            <div className="container">
+                <div className="section-header text-center">
+                    <p>Our Team</p>
+                    <h2>Meet Our Experts</h2>
+                </div>
+                <div className="row">
+                    <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                        <div className="team-item">
+                            <div className="team-img">
+                                <img src="img/team-1.jpg" alt="Team Image" />
+                            </div>
+                            <div className="team-text">
+                                <h2>Prakash K</h2>
+                                <p>Founder & CEO</p>
+                            </div>
+                            <div className="team-social">
+                                <a className="social-tw" href=""><i className="fab fa-twitter"></i></a>
+                                <a className="social-fb" href=""><i className="fab fa-facebook-f"></i></a>
+                                <a className="social-li" href=""><i className="fab fa-linkedin-in"></i></a>
+                                <a className="social-in" href=""><i className="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.2s">
+                        <div className="team-item">
+                            <div className="team-img">
+                                <img src="img/team-2.jpeg" alt="Team Image" />
+                            </div>
+                            <div className="team-text">
+                                <h2>Sirancheevi P</h2>
+                                <p>Founder & CTO</p>
+                            </div>
+                            <div className="team-social">
+                                <a className="social-tw" href=""><i className="fab fa-twitter"></i></a>
+                                <a className="social-fb" href=""><i className="fab fa-facebook-f"></i></a>
+                                <a className="social-li" href=""><i className="fab fa-linkedin-in"></i></a>
+                                <a className="social-in" href=""><i className="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                        <div className="team-item">
+                            <div className="team-img">
+                                <img src="img/team-3.jpeg" alt="Team Image" />
+                            </div>
+                            <div className="team-text">
+                                <h2>Akash</h2>
+                                <p>Accounts head</p>
+                            </div>
+                            <div className="team-social">
+                                <a className="social-tw" href=""><i className="fab fa-twitter"></i></a>
+                                <a className="social-fb" href=""><i className="fab fa-facebook-f"></i></a>
+                                <a className="social-li" href=""><i className="fab fa-linkedin-in"></i></a>
+                                <a className="social-in" href=""><i className="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.4s">
+                        <div className="team-item">
+                            <div className="team-img">
+                                <img src="img/team-4.jpeg" alt="Team Image" />
+                            </div>
+                            <div className="team-text">
+                                <h2>Sankar</h2>
+                                <p>Senior web developer</p>
+                            </div>
+                            <div className="team-social">
+                                <a className="social-tw" href=""><i className="fab fa-twitter"></i></a>
+                                <a className="social-fb" href=""><i className="fab fa-facebook-f"></i></a>
+                                <a className="social-li" href=""><i className="fab fa-linkedin-in"></i></a>
+                                <a className="social-in" href=""><i className="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Team;

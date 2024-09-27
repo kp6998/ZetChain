32a0f47951c73941aae73ed7e28a63713bed1813
kp6998 <kp06091998@gmail.com>
2024-09-27 16:29:07 +0530

    firebase update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,13 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+  entry: './src/index.js', // Make sure to update the entry point if necessary
+  output: {
+    path: path.resolve(__dirname, 'build'), // Set output folder to 'build'
+    filename: 'bundle.js', // Customize the output filename if needed
+    publicPath: '/', // Set the base path for all assets
+  },
   module: {
     rules: [
       {
@@ -18,13 +26,17 @@ module.exports = {
         use: [
           {
             loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'assets', // Store images in 'assets' folder
+            },
           },
         ],
       },
     ],
   },
   devServer: {
-    historyApiFallback: true, // this is for react router
+    historyApiFallback: true, // this is for React Router
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -32,4 +44,7 @@ module.exports = {
       filename: './index.html',
     }),
   ],
+  resolve: {
+    extensions: ['.js', '.jsx'], // Add any file extensions you're working with
+  },
 };

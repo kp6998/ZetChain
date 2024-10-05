49e8cce62decf1f337a4ba6d634d834613bf3986
kp6998 <kp06091998@gmail.com>
2024-10-05 19:39:31 +0530

    changes teams

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,62 +1,67 @@
-    import React, { useState, useEffect } from 'react';
-    import { db} from '../Firebase';
-    import {  getDocs,collection } from 'firebase/firestore'; // Import getDocs from Firestore
-    import { onValue, ref } from 'firebase/database';
+import React, { useState, useEffect } from 'react';
+import { db } from '../Firebase';
+import { getDocs, collection } from 'firebase/firestore'; // Import getDocs from Firestore
+import { onValue, ref } from 'firebase/database';
 
-    const Team = () => {
+const Team = () => {
     const [teamData, setTeamData] = useState([]);
 
     useEffect(() => {
         const query = ref(db, "Tharkuri Tech/Teams");
         return onValue(query, (snapshot) => {
-          const data = snapshot.val();
-    
-          if (snapshot.exists()) {
-            setTeamData(data);
-          }
+            const data = snapshot.val();
+
+            if (snapshot.exists()) {
+                setTeamData(data);
+            }
         });
-      }, []);
+    }, []);
 
     return (
         <div className="team">
-        <div className="container">
-            <div className="section-header text-center">
-            <p>Our Team</p>
-            <h2>Meet Our Experts</h2>
-            </div>
-            <div className="row">
-            {teamData.map((member, index) => (
-                <div
-                className="col-lg-3 col-md-6 wow fadeInUp"
-                data-wow-delay={`${0.1 * (index + 1)}s`}
-                key={index}
-                >
-                <div className="team-item">
-                    <div className="team-img">
-                    <img src={member.image} alt={`${member.name} Image`} />
-                    </div>
-                    <div className="team-text">
-                    <h2>{member.name}</h2>
-                    <p>{member.designation}</p>
-                    </div>
-                    <div className="team-social">
-                    <a className="social-tw" href={member.twitter} target='_blank' rel="noreferrer">
-                        <i className="fab fa-twitter"></i>
-                    </a>
-                    <a className="social-li" href={member.linkedin} target='_blank' rel="noreferrer"> 
-                        <i className="fab fa-linkedin-in"></i>
-                    </a>
-                    <a className="social-in" href={member.instagram} target='_blank' rel="noreferrer">
-                        <i className="fab fa-instagram"></i>
-                    </a>
-                    </div>
+            <div className="container">
+                <div className="section-header text-center">
+                    <p>Our Team</p>
+                    <h2>Meet Our Experts</h2>
                 </div>
+                <div className="row">
+                    {teamData.map((member, index) => (
+                        <div
+                            className="col-lg-3 col-md-6 wow fadeInUp"
+                            data-wow-delay={`${0.1 * (index + 1)}s`}
+                            key={index}
+                        >
+                            <div className="team-item">
+                                <div className="team-img">
+                                    <img src={member.image} alt={`${member.name} Image`} />
+                                </div>
+                                <div className="team-text">
+                                    <h2>{member.name}</h2>
+                                    <p>{member.designation}</p>
+                                </div>
+                                <div className="team-social">
+                                    <a className="social-tw" href={member.twitter} target='_blank' rel="noreferrer">
+                                        <i className="fab fa-twitter"></i>
+                                    </a>
+                                    <a className="social-li" href={member.linkedin} target='_blank' rel="noreferrer">
+                                        <i className="fab fa-linkedin-in"></i>
+                                    </a>
+                                    <a className="social-in" href={member.instagram} target='_blank' rel="noreferrer">
+                                        <i className="fab fa-instagram"></i>
+                                    </a>
+                                    {member.portfolio != null &&
+                                        <a className="social-in" href={member.portfolio} target='_blank' rel="noreferrer">
+                                            <i className="fas fa-globe"></i>
+                                        </a>
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-            ))}
             </div>
-        </div>
         </div>
     );
-    };
+};
 
-    export default Team;
+export default Team;
